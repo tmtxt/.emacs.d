b57cf78162f6d7db010491f00687b8143c8098ea
tmtxt <tmtxt@tmtxt.local>
2013-12-21 20:00:15 +0700

    key bindings for javascript mode

--- config/tmtxt-javascript.el
+++ config/tmtxt-javascript.el
@@ -4,4 +4,11 @@
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
+;;; key bindings
+(define-key js-mode-map (kbd "M-j") 'js2-line-break)
+(define-key js-mode-map (kbd "C-M-?") 'ac-js2-jump-to-definition)
+(define-key js-mode-map (kbd "C-M-\"") 'js2-mark-defun)
+(define-key js-mode-map (kbd "C-M-:") 'js2-mode-toggle-hide-functions)
+(define-key js-mode-map (kbd "C-M->") 'js2-mode-toggle-element)
+
 (provide 'tmtxt-javascript)
