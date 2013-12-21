d10bc5f6c626beb199a373027d99db261a5ef50e
tmtxt <tmtxt@tmtxt.local>
2013-12-21 22:00:14 +0700

    bind key on hook only

--- config/tmtxt-javascript.el
+++ config/tmtxt-javascript.el
@@ -5,10 +5,13 @@
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
 ;;; key bindings
-(define-key js-mode-map (kbd "M-j") 'js2-line-break)
-(define-key js-mode-map (kbd "C-M-?") 'ac-js2-jump-to-definition)
-(define-key js-mode-map (kbd "C-M-\"") 'js2-mark-defun)
-(define-key js-mode-map (kbd "C-M-:") 'js2-mode-toggle-hide-functions)
-(define-key js-mode-map (kbd "C-M->") 'js2-mode-toggle-element)
+(add-hook 'js-mode-hook
+		  (lambda ()
+			(define-key js-mode-map (kbd "M-j") 'js2-line-break)
+			(define-key js-mode-map (kbd "C-M-?") 'ac-js2-jump-to-definition)
+			(define-key js-mode-map (kbd "C-M-\"") 'js2-mark-defun)
+			(define-key js-mode-map (kbd "C-M-:") 'js2-mode-toggle-hide-functions)
+			(define-key js-mode-map (kbd "C-M->") 'js2-mode-toggle-element)))
+
 
 (provide 'tmtxt-javascript)
