d8882b0f1aea0db6597f9ce8cafc4df858a42556
tmtxt <tmtxt@tmtxt.local>
2014-01-12 16:25:42 +0700

    config for eshell

--- config/tmtxt-shell.el
+++ config/tmtxt-shell.el
@@ -1,6 +1,16 @@
 ;;; config for working with shell command in emacs
 
-;;; copy PATH from shell
+;;; copy PATH from my default shell
 (exec-path-from-shell-initialize)
+
+;;; eshell
+(setq eshell-save-history-on-exit t)	;save history
+(setq eshell-cmpl-cycle-completions t)	;TAB for suggestion
+(setq eshell-buffer-shorthand t)		;shorthand buffer name
+
+;;; key binding for eshell
+(global-set-key (kbd "C-x m") 'eshell)
+;; Start a new eshell even if one is active.
+(global-set-key (kbd "C-x C-m") (lambda () (interactive) (eshell t)))
 
 (provide 'tmtxt-shell)
