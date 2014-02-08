9291f683a48d9844a16e54c95bf275366393b2f0
tmtxt <tmtxt@tmtxt.local>
2014-02-08 16:24:40 +0700

    some key bindings for org mode

--- config/tmtxt-org.el
+++ config/tmtxt-org.el
@@ -5,13 +5,28 @@
 
 ;;; some recomended settings from org mode manual
 (add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(add-hook 'org-mode-hook (tmtxt/on-fn 'org-indent-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
 
 ;;; rebinding keys
+;; (define-key org-mode-map (kbd "C-S-j") nil)
+;; (define-key org-mode-map (kbd "C-S-l") nil)
 (global-unset-key (kbd "C-M-j"))
 (global-unset-key (kbd "C-M-l"))
+
+;;; navigation between entries
+(define-key org-mode-map (kbd "C-M-S-k") 'outline-next-visible-heading)
+(define-key org-mode-map (kbd "C-M-S-i") 'outline-previous-visible-heading)
+(define-key org-mode-map (kbd "C-M-S-l") 'org-forward-heading-same-level)
+(define-key org-mode-map (kbd "C-M-S-j") 'org-backward-heading-same-level)
+(define-key org-mode-map (kbd "C-M-S-u") 'outline-up-heading)
+(define-key org-mode-map (kbd "<C-tab>") 'ido-switch-buffer)
+
+;;;
+(setq
+ org-src-fontify-natively t)
 
 ;;; move meta up/down/left/right
 (define-key org-mode-map (kbd "C-s-j") 'org-metaleft)
