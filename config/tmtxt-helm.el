c787e417efc92dfdb658627faaffbe151c7423c7
tmtxt <tmtxt@tmtxt.local>
2014-02-08 16:24:08 +0700

    fix some key bindings with helm

--- config/tmtxt-helm.el
+++ config/tmtxt-helm.el
@@ -5,13 +5,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; helm locate
 (setq helm-locate-command "mdfind %s %s")
-(global-set-key (kbd "C-M-l") 'helm-locate)
+;; (global-set-key (kbd "C-M-l") 'helm-locate)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; helm swoop
 ;; (global-set-key (kbd "M-i") 'helm-swoop)
 ;; (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
-;; (define-key isearch-mode-map (kbd "M-i") 'helm-swoop-from-isearch)
+;; (define-key isearch-mode-map (kbd "C-w") 'helm-swoop-from-isearch)
 
 ;;; finally provide the library
 (provide 'tmtxt-helm)
