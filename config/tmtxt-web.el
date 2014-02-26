bf8acf445eee4ed29007f55e0cd85d4694509bc2
tmtxt <tmtxt@Trans-MacBook-Pro.local>
2014-02-26 21:07:56 +0700

    associate .ejs with web mode

--- config/tmtxt-web.el
+++ config/tmtxt-web.el
@@ -29,6 +29,7 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.xml?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs?\\'" . web-mode))
 
 ;;; disable rainbow-mode and whitespace-mode when use web-mode
 (defun web-mode-hook ()
