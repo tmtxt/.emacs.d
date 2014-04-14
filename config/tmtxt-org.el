669efd63ae6fb0c8ea08f6d0fd99810824fda4a3
tmtxt <tmtxt@tmtxt-mac.local>
2014-04-14 18:24:42 +0700

    add some config for org mode

--- config/tmtxt-org.el
+++ config/tmtxt-org.el
@@ -8,7 +8,31 @@
 (add-hook 'org-mode-hook (tmtxt/on-fn 'org-indent-mode))
 
 (setq
- org-src-fontify-natively t)
+ ;; fontify code block
+ org-src-fontify-natively t
+
+ ;; not allow to edit in the foded region
+ org-catch-invisible-edits 'error
+
+ ;; subscript and superscript
+ org-use-sub-superscripts '{}
+ org-export-with-sub-superscripts '{}
+
+ ;; M-Return (insert new heading at same level)
+ org-M-RET-may-split-line nil           ;don't split line
+ org-insert-heading-respect-content t   ;insert after the content
+
+ ;; indentation
+ org-indent-indentation-per-level 1
+ org-list-indent-offset 1
+
+ ;; org goto
+ org-goto-auto-isearch nil
+ org-goto-interface 'outline-path-completion
+
+ ;; special C-a, C-e, C-k
+ org-special-ctrl-a/e t
+ org-special-ctrl-k t)
 
 (dolist (hook '(org-mode-hook))
   (add-hook hook (lambda () (flyspell-mode 1))))
