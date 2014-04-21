eed082906be8d92f6081b2bcf50ab5646800e43f
tmtxt <tmtxt@tmtxt-mac.local>
2014-04-21 22:17:05 +0700

    jj to quit evil insert state

--- config/tmtxt-evil.el
+++ config/tmtxt-evil.el
@@ -10,4 +10,14 @@
 (dolist (mode '(dired-mode))
   (add-to-list 'evil-motion-state-modes mode))
 
+;;; use jj to switch from insert state to normal or motion state 
+(tmtxt/set-up 'key-chord
+  (key-chord-mode 1)
+  (defun tmtxt/evil-exit-insert-state ()
+    "Exit evil insert state and change to normal or motion mode"
+    (interactive)
+    (if (member major-mode '(dired-mode))
+        (evil-motion-state 1)
+      (evil-normal-state 1)))
+  (key-chord-define evil-emacs-state-map "jj" 'tmtxt/evil-exit-insert-state))
 (provide 'tmtxt-evil)
