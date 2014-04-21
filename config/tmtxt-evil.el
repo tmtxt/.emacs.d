9d2f753ce95f75c0c127701d159d00c242d864ac
tmtxt <tmtxt@tmtxt-mac.local>
2014-04-21 22:16:49 +0700

    default evil state for certain modes

--- config/tmtxt-evil.el
+++ config/tmtxt-evil.el
@@ -3,6 +3,11 @@
 ;;; not use evil insert state, prefer emacs state
 (add-hook 'evil-insert-state-entry-hook (lambda () (evil-emacs-state 1)))
 
+;;; default state
+(dolist (mode '(git-commit-mode))
+  (add-to-list 'evil-insert-state-modes mode))
 
+(dolist (mode '(dired-mode))
+  (add-to-list 'evil-motion-state-modes mode))
 
 (provide 'tmtxt-evil)
