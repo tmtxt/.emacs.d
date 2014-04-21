230108df7a0803826159ab9022a50aec01b3ea1f
tmtxt <tmtxt@tmtxt-mac.local>
2014-04-21 22:34:24 +0700

    color indicators for evil mode

--- config/tmtxt-evil.el
+++ config/tmtxt-evil.el
@@ -33,4 +33,30 @@
     (goto-char (point-max))))
 (add-hook 'evil-emacs-state-entry-hook 'tmtxt/repl-goto-prompt)
 
+;;; evil indicator
+(defface tmtxt/evil-insert-tag
+  `((t (:inherit font-lock-comment-delimiter-face :slant normal :weight bold)))
+  "Evil insert mode indicator face")
+(defface tmtxt/evil-normal-tag
+  `((t (:inherit diredp-mode-line-flagged :weight bold :foreground "#C88BA7")))
+  "Evil normal mode indicator face")
+(defface tmtxt/evil-emacs-tag
+  `((t (:inherit diredp-mode-line-marked :weight bold :foreground "#2E9B98")))
+  "Evil emacs mode indicator face")
+(defface tmtxt/evil-visual-tag
+  `((t (:inherit font-lock-preprocessor-face)))
+  "Evil visual mode indicator face")
+
+(setq evil-mode-line-format 'before
+      evil-normal-state-tag (propertize "« N »" 'face 'tmtxt/evil-normal-tag)
+      evil-motion-state-tag (propertize "« M »" 'face 'tmtxt/evil-normal-tag)
+      evil-insert-state-tag (propertize "« I »" 'face 'tmtxt/evil-insert-tag)
+      evil-emacs-state-tag (propertize "« E »" 'face 'tmtxt/evil-emacs-tag)
+      evil-visual-state-tag (propertize "« ∞ »" 'face 'tmtxt/evil-visual-tag)
+      evil-motion-state-cursor '(box "YellowGreen")
+      evil-normal-state-cursor '(box "YellowGreen")
+      evil-insert-state-cursor '(bar "YellowGreen")
+      evil-emacs-state-cursor '(bar "YellowGreen")
+      evil-visual-state-cursor '(box "#F86155"))
+
 (provide 'tmtxt-evil)
