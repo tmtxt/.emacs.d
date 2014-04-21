7d9e780030d0ef08e12316e7bbf8dba1814cdd62
tmtxt <tmtxt@tmtxt-mac.local>
2014-04-21 15:29:30 +0700

    change helm locate command to mdfind only in osx

--- config/tmtxt-helm.el
+++ config/tmtxt-helm.el
@@ -1,21 +1,12 @@
 ;;; config for helm
 
+;;; enable helm
 (helm-mode 1)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; helm locate
-(setq helm-locate-command "mdfind %s %s")
-;; (global-set-key (kbd "C-M-l") 'helm-locate)
+(tmtxt/in '(darwin)
+  (setq helm-locate-command "mdfind %s %s"))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; helm recent dirs
-(require 'helm-dired-recent-dirs)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; helm swoop
-;; (global-set-key (kbd "M-i") 'helm-swoop)
-;; (global-set-key (kbd "M-I") 'helm-swoop-back-to-last-point)
-;; (define-key isearch-mode-map (kbd "C-w") 'helm-swoop-from-isearch)
+(tmtxt/set-up 'helm-dired-recent-dirs)
 
 ;;; finally provide the library
 (provide 'tmtxt-helm)
