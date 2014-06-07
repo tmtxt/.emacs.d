0b14470520ed2d0d74c7e46abb89fb5182743613
tmtxt <tmtxt@tmtxt-mac.local>
2014-06-07 22:44:08 +0700

    auto enable emmet mode for web mode

--- config/tmtxt-web.el
+++ config/tmtxt-web.el
@@ -10,6 +10,7 @@
 ;;; auto start on sgml and css mode
 (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
 (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+(add-hook 'web-mode-hook 'emmet-mode)
 
 ;;; default indentation for html mode
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;indent 2 spaces.
