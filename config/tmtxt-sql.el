d3ecc57fc6edaa7dc320e9a03bcee0b298a4be78
Truong <truong@cogini.com>
2014-08-20 23:32:04 +0700

    add commands for connecting sql dev servers

--- config/tmtxt-sql.el
+++ config/tmtxt-sql.el
@@ -7,6 +7,30 @@
 
   (add-hook 'sql-interactive-mode-hook
             (lambda ()
-              (toggle-truncate-lines t))))
+              (toggle-truncate-lines t)))
+
+  (setq sql-connection-alist
+        '((icon.dev (sql-product 'postgres)
+                    (sql-port 54321)
+                    (sql-server "localhost")
+                    (sql-user "icon")
+                    (sql-password "vagrant")
+                    (sql-database "icon"))
+          (mooc.dev (sql-product 'postgres)
+                    (sql-port 54321)
+                    (sql-server "localhost")
+                    (sql-user "mooc")
+                    (sql-password "vagrant")
+                    (sql-database "mooc_dev"))))
+
+  (defun tmtxt/sql-icon-dev ()
+    (interactive)
+    (setq sql-product 'postgres)
+    (sql-connect 'icon.dev))
+
+  (defun tmtxt/sql-mooc-dev ()
+    (interactive)
+    (setq sql-product 'postgres)
+    (sql-connect 'mooc.dev)))
 
 (provide 'tmtxt-sql)
