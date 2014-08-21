926ac9cd859c76c00bd9d6981bd3888888b206ab
Truong <truong@cogini.com>
2014-08-22 00:55:49 +0700

    read sql password from identica file

--- config/tmtxt-sql.el
+++ config/tmtxt-sql.el
@@ -1,36 +1,48 @@
-(tmtxt/set-up 'sql
-  (setq sql-postgres-login-params
-        '((user :default "postgres")
-          (database :default "postgres")
-          (server :default "localhost")
-          (port :default 5432)))
+(setq sql-postgres-login-params
+      '((user :default "postgres")
+        (database :default "postgres")
+        (server :default "localhost")
+        (port :default 5432)))
 
-  (add-hook 'sql-interactive-mode-hook
-            (lambda ()
-              (toggle-truncate-lines t)))
+(add-hook 'sql-interactive-mode-hook
+          (lambda ()
+            (toggle-truncate-lines t)))
 
-  (setq sql-connection-alist
-        '((icon.dev (sql-product 'postgres)
-                    (sql-port 54321)
-                    (sql-server "localhost")
-                    (sql-user "icon")
-                    (sql-password "vagrant")
-                    (sql-database "icon"))
-          (mooc.dev (sql-product 'postgres)
-                    (sql-port 54321)
-                    (sql-server "localhost")
-                    (sql-user "mooc")
-                    (sql-password "vagrant")
-                    (sql-database "mooc_dev"))))
+(setq sql-connection-alist
+      '((mooc.dev (sql-product 'postgres)
+                  (sql-port 54321)
+                  (sql-server "localhost")
+                  (sql-user "mooc")
+                  (sql-database "mooc_dev"))
+        (icon.dev (sql-product 'postgres)
+                  (sql-port 54321)
+                  (sql-server "localhost")
+                  (sql-user "icon")
+                  (sql-database "icon"))))
 
-  (defun tmtxt/sql-icon-dev ()
-    (interactive)
-    (setq sql-product 'postgres)
-    (sql-connect 'icon.dev))
+(defun tmtxt/sql-icon-dev ()
+  (interactive)
+  (tmtxt/sql-connect 'postgres 'icon.dev))
 
-  (defun tmtxt/sql-mooc-dev ()
-    (interactive)
-    (setq sql-product 'postgres)
-    (sql-connect 'mooc.dev)))
+(defun tmtxt/sql-mooc-dev ()
+  (interactive)
+  (tmtxt/sql-connect 'postgres 'mooc.dev))
+
+(defun tmtxt/sql-connect (product connection)
+  ;; load the password
+  (require 'tmtxt-identica "tmtxt-identica.el.gpg")
+
+  ;; update the password to the sql-connection-alist
+  (let ((connection-info (assoc connection sql-connection-alist))
+        (sql-password (car (last (assoc connection tmtxt-sql-password))))
+        new-connection-alist)
+    (delete sql-password connection-info)
+    (nconc connection-info `((sql-password ,sql-password)))
+    (setq sql-connection-alist (assq-delete-all connection sql-connection-alist))
+    (add-to-list 'sql-connection-alist connection-info))
+
+  ;; connect to database
+  (setq sql-product product)
+  (sql-connect connection))
 
 (provide 'tmtxt-sql)
