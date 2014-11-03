83772e186c5614459642465bebdc835adc5ce6d6
Truong <truong@cogini.com>
2014-11-03 22:32:19 +0700

    arrange package source

--- init.el
+++ init.el
@@ -16,10 +16,8 @@
 (require 'package)
 (dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
                   ("elpa" . "http://tromey.com/elpa/")
-                  ;; TODO: Maybe, use this after emacs24 is released
-                  ;; (development versions of packages)
                   ("melpa" . "http://melpa.milkbox.net/packages/")
-                  ))
+                  ("melpa-stable" . "http://melpa-stable.milkbox.net/packages/")))
   (add-to-list 'package-archives source t))
 (package-initialize)
 
