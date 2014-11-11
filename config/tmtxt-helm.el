51cbbc30c6760a650bf5532c6a7cf0999df1bb09
Truong <truong@cogini.com>
2014-11-11 18:37:15 +0700

    not use thing at point as default input for helm

--- config/tmtxt-helm.el
+++ config/tmtxt-helm.el
@@ -20,7 +20,7 @@
 (setq
  helm-mp-highlight-delay 0.7
  helm-mp-highlight-threshold 4
- helm-maybe-use-default-as-input t
+ helm-maybe-use-default-as-input nil
  helm-quick-update t
  helm-idle-delay 0.01
  helm-input-idle-delay 0.01
