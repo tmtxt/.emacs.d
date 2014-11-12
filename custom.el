4a7ee5f230e1fcc1f406b43b293a5e901b619059
Truong <truong@cogini.com>
2014-11-12 19:13:20 +0700

    emacs auto updates custom.el

--- custom.el
+++ custom.el
@@ -5,11 +5,15 @@
  ;; If there is more than one, they won't work right.
  '(ac-ispell-requires 4)
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
- '(custom-safe-themes (quote ("e4b1468bc1def510a4157664603f2edfe9b57551a30969040f1d9313a3ee0b7b" "e2a8b097cc1b7e691657220bf19a84f0607d714a6417f679073c36b81ffe9d0d" default)))
+ '(custom-safe-themes
+   (quote
+    ("e4b1468bc1def510a4157664603f2edfe9b57551a30969040f1d9313a3ee0b7b" "e2a8b097cc1b7e691657220bf19a84f0607d714a6417f679073c36b81ffe9d0d" default)))
  '(ecb-options-version "2.40")
  '(ergoemacs-mode-used "5.7.5")
  '(ergoemacs-theme "5.7.5")
- '(quack-programs (quote ("mit-scheme-x86-64" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi"))))
+ '(quack-programs
+   (quote
+    ("mit-scheme-x86-64" "bigloo" "csi" "csi -hygienic" "gosh" "gracket" "gsi" "gsi ~~/syntax-case.scm -" "guile" "kawa" "mit-scheme" "racket" "racket -il typed/racket" "rs" "scheme" "scheme48" "scsh" "sisc" "stklos" "sxi"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
