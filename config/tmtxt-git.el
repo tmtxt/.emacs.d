33ab8a63b24dd80ec455241f2c6b9514b7333964
Truong <truong@cogini.com>
2014-11-15 14:59:08 +0700

    use pop up window for magit functions

--- config/tmtxt-git.el
+++ config/tmtxt-git.el
@@ -5,10 +5,10 @@
 (add-hook 'git-commit-mode-hook
           (lambda () (flyspell-mode 0)) t)
 
-(setq magit-status-buffer-switch-function 'switch-to-buffer)
+;; (setq magit-status-buffer-switch-function 'switch-to-buffer)
 (add-hook 'magit-mode-hook 'magit-load-config-extensions)
 (set-default 'magit-stage-all-confirm nil)
 (setq magit-use-overlays nil)
-(setq magit-server-window-for-commit 'switch-to-buffer)
+;; (setq magit-server-window-for-commit 'switch-to-buffer)
 
 (provide 'tmtxt-git)
