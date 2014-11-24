b7ebcbf83a30a3207e0b39bfc93daefa8df4092f
Truong <truong@cogini.com>
2014-11-24 22:32:33 +0700

    update connect sql servers commands

--- config/tmtxt-sql.el
+++ config/tmtxt-sql.el
@@ -30,24 +30,6 @@
                        (sql-user "sugar_and_sea")
                        (sql-database "sugar_and_sea"))))
 
-(defun tmtxt/sql-connect (product connection)
-  ;; load the password
-  (require 'tmtxt-identica "tmtxt-identica.el.gpg")
-
-  ;; update the password to the sql-connection-alist
-  (let ((connection-info (assoc connection sql-connection-alist))
-        (sql-password (car (last (assoc connection tmtxt-sql-password)))))
-    (delete sql-password connection-info)
-    (nconc connection-info `((sql-password ,sql-password)))
-    (setq sql-connection-alist (assq-delete-all connection sql-connection-alist))
-    (add-to-list 'sql-connection-alist connection-info))
-
-  ;; connect to database
-  (setq sql-product product)
-  (if current-prefix-arg
-      (sql-connect connection connection)
-    (sql-connect connection)))
-
 (defun tmtxt/sql-connect-server (connection)
   "Connect to the input server using tmtxt/sql-servers-list"
   (interactive
@@ -56,11 +38,27 @@
                                                 (symbol-name (nth 0 item))
                                                 (nth 0 item)))
                                              sql-connection-alist)))
+  ;; password
+  (require 'tmtxt-identica "tmtxt-identica.el.gpg")
   ;; get the sql connection info and product from the sql-connection-alist
   (let* ((connection-info (assoc connection sql-connection-alist))
-         (connection-product (nth 1 (nth 1 (assoc 'sql-product connection-info)))))
-    ;; connect to server
-    (tmtxt/sql-connect connection-product connection)))
+         (connection-product (nth 1 (nth 1 (assoc 'sql-product connection-info))))
+         (sql-password (nth 1 (assoc connection tmtxt-sql-password))))
+    ;; delete the connection info from the sql-connection-alist
+    (setq sql-connection-alist (assq-delete-all connection sql-connection-alist))
+    ;; delete the old password from the connection-info
+    (setq connection-info (assq-delete-all 'sql-password connection-info))
+    ;; add the password to the connection-info
+    (nconc connection-info `((sql-password ,sql-password)))
+    ;; add back the connection info to the beginning of sql-connection-alist
+    ;; (last used server will appear first for the next prompt)
+    (add-to-list 'sql-connection-alist connection-info)
+    ;; override the sql-product by the product of this connection
+    (setq sql-product connection-product)
+    ;; connect
+    (if current-prefix-arg
+        (sql-connect connection connection)
+      (sql-connect connection))))
 
 ;;; sql up mode
 ;; (tmtxt/set-up 'sqlup-mode
