dd0659b7ce25c9771d159f2d3e89937297fa65a0
Truong <me@truongtx.me>
2015-04-15 10:31:32 +0700

    update bookmark+, cider

--- elpa/bookmark+-20141115.2050/bookmark+-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "bookmark+" "20141115.2050" "Bookmark+: extensions to standard library `bookmark.el'." 'nil :url "http://www.emacswiki.org/bookmark+.el" :keywords
-  '("bookmarks" "bookmark+" "placeholders" "annotations" "search" "info" "url" "w3m" "gnus"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
