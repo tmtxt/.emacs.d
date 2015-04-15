a11937b06e64c4f0a504339ee339cb341aef948d
Truong <me@truongtx.me>
2015-04-15 10:01:26 +0700

    install elixir mode

---
+++ elpa/elixir-mode-2.2.0/elixir-mode-pkg.el
@@ -0,0 +1,5 @@
+(define-package "elixir-mode" "2.2.0" "Major mode for editing Elixir files" 'nil :url "https://github.com/elixir-lang/emacs-elixir" :keywords
+  '("languages" "elixir"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
