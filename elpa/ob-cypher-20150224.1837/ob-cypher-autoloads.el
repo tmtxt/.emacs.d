65b90e81a013860c00cdc5f4e5f6112e5f90db83
Truong <me@truongtx.me>
2015-05-15 21:39:06 +0700

    install cypher-mode and ob-cypher

---
+++ elpa/ob-cypher-20150224.1837/ob-cypher-autoloads.el
@@ -0,0 +1,15 @@
+;;; ob-cypher-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("ob-cypher.el") (21846 1135 297633 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ob-cypher-autoloads.el ends here
