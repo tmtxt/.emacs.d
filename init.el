8f486bc70ad063d62c5dedf28e5ad425efa3b912
Truong <me@truongtx.me>
2015-07-01 23:49:30 +0700

    function for sending region or buffer

--- init.el
+++ init.el
@@ -317,5 +317,12 @@
          (string (buffer-substring-no-properties beg end)))
     (n4s-send-string string)))
 
+(defun n4s-send-region-or-buffer ()
+  "Send the selected region if presented, otherwise, send the whole buffer"
+  (interactive)
+  (if (use-region-p)
+      (n4s-send-region)
+    (n4s-send-buffer)))
+
 (setq n4s-cli-program "vagrant")
 (setq n4s-cli-arguments '("ssh" "-c" "/home/vagrant/neo4j/neo4j-community-2.2.1/bin/neo4j-shell -port 7475"))
