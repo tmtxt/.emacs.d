40e756440e04c5a868862b7fa2b74c45a7f5d839
Tran Xuan Truong <me@truongtx.me>
2015-08-27 10:08:07 +0700

    remove save history

--- config/tmtxt-sql.el
+++ config/tmtxt-sql.el
@@ -59,25 +59,4 @@
         (sql-connect connection connection)
       (sql-connect connection))))
 
-;;; sql up mode
-;; (tmtxt/set-up 'sqlup-mode
-;;   (add-hook 'sql-mode-hook 'sqlup-mode)
-;;   (add-hook 'sql-interactive-mode-hook 'sqlup-mode))
-
-(when (not (file-exists-p "~/.emacs.d/sql-history"))
-  (make-directory "~/.emacs.d/sql-history"))
-(defun tmtxt/sql-save-history-hook ()
-  (let ((lval 'sql-input-ring-file-name)
-        (rval 'sql-product))
-    (if (symbol-value rval)
-        (let ((filename
-               (concat "~/.emacs.d/sql-history/"
-                       (symbol-name (symbol-value rval))
-                       "-history.sql")))
-          (set (make-local-variable lval) filename))
-      (error
-       (format "SQL history will not be saved because %s is nil"
-               (symbol-name rval))))))
-(add-hook 'sql-interactive-mode-hook 'tmtxt/sql-save-history-hook)
-
 (provide 'tmtxt-sql)
