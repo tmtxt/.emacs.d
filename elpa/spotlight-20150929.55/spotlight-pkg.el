ce7e2d7d6be004c2d16607dc824eed11d985c37c
Tran Xuan Truong <truong.tran@lazada.com>
2015-10-22 16:12:13 +0700

    install spotlight for osx

---
+++ elpa/spotlight-20150929.55/spotlight-pkg.el
@@ -0,0 +1,1 @@
+(define-package "spotlight" "20150929.55" "search files with Mac OS X spotlight" '((emacs "24.1") (swiper "0.6.0") (counsel "0.6.0")) :url "http://www.pragmaticemacs.com" :keywords '("search" "external"))
