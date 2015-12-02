1fbbc4839aad015a907aab6e34c072ddc6e8b094
Tran Xuan Truong <truong.tran@lazada.com>
2015-12-02 11:45:54 +0700

    not show trailing whitespace in eshell

--- config/tmtxt-shell.el
+++ config/tmtxt-shell.el
@@ -1,5 +1,9 @@
-;;; config for working with shell command in emacs
+;;; shell --- config for using shell in emacs
 
+;;; Commentary:
+;;; Author: truongtx
+
+;;; Code:
 ;;; copy PATH from my default shell
 (tmtxt/set-up 'exec-path-from-shell
   (exec-path-from-shell-initialize))
@@ -9,4 +13,9 @@
 (setq eshell-cmpl-cycle-completions t)	;TAB for suggestion
 (setq eshell-buffer-shorthand t)		;shorthand buffer name
 
+(add-hook 'eshell-mode-hook
+          (lambda ()
+            (setq-local show-trailing-whitespace nil)))
+
 (provide 'tmtxt-shell)
+;;; tmtxt-shell.el ends here
