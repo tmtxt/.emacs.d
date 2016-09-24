adddb8001232ad57987239a3f9bc66c68b451bfd
Truong <me@truongtx.me>
2016-09-24 21:15:54 +0700

    update packages

--- config/tmtxt-bookmark.el
+++ config/tmtxt-bookmark.el
@@ -1,7 +1,11 @@
 ;;; this is my config for bookmark and bookmark+
 
-;;; require package
+;;; require
 (require 'bookmark+)
+
+(setq
+ bookmark-default-file "~/emacs.bookmarks"
+ bmkp-last-as-first-bookmark-file "~/emacs.bookmarks")
 
 ;;; put last-selected bookmark on top
 ;;; http://www.emacswiki.org/emacs/BookMarks#toc6
@@ -9,10 +13,6 @@
   (let ((latest (bookmark-get-bookmark bookmark)))
     (setq bookmark-alist (delq latest bookmark-alist))
     (add-to-list 'bookmark-alist latest)))
-
-(setq
- bookmark-default-file "~/emacs.bookmarks"
- bmkp-last-as-first-bookmark-file "~/emacs.bookmarks")
 
 ;;; finally provide the library to call
 (provide 'tmtxt-bookmark)
