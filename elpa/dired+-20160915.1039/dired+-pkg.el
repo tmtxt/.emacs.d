78534d776d0ee01ef0d15b35264e6d7c285b0e2e
Truong <me@truongtx.me>
2016-09-24 16:12:29 +0700

    update packages

--- elpa/dired+-20160915.1039/dired+-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dired+" "20160915.1039" "Extensions to Dired." 'nil :url "http://www.emacswiki.org/dired+.el" :keywords '("unix" "mouse" "directories" "diredp" "dired"))
--- elpa/projectile-20160917.2336/projectile-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "projectile" "20160917.2336" "Manage and navigate projects in Emacs easily" '((pkg-info "0.4")) :url "https://github.com/bbatsov/projectile" :keywords '("project" "convenience"))
