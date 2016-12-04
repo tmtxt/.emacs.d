91b624ef787c76212a503fccc813d916bd227054
TruongTX <tmtxt@truong-book.local>
2016-12-04 19:00:46 +0700

    config ruby-end for elixir mode

--- config/tmtxt-elixir.el
+++ config/tmtxt-elixir.el
@@ -1,3 +1,11 @@
+(require 'ruby-end)
+(require 'elixir-mode)
 
+(add-to-list 'elixir-mode-hook
+             (defun auto-activate-ruby-end-mode-for-elixir-mode ()
+               (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
+                    "\\(?:^\\|\\s-+\\)\\(?:do\\)")
+               (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
+               (ruby-end-mode +1)))
 
 (provide 'tmtxt-elixir)
---
+++ elpa/ruby-end-0.4.1/ruby-end-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ruby-end" "0.4.1" "Automatic insertion of end blocks for Ruby" 'nil :url "http://github.com/rejeep/ruby-end" :keywords '("speed" "convenience" "ruby"))
