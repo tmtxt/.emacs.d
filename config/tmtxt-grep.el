8ca8d3ee28256d7ad5d98fce76373b80f567a74b
TruongTX <me@truongtx.me>
2018-02-01 21:19:44 +0700

    Update snippets

--- config/tmtxt-grep.el
+++ config/tmtxt-grep.el
@@ -4,8 +4,10 @@
 
 ;;; ignore from grep
 (add-to-list 'grep-find-ignored-files "*.class")
-     (add-to-list 'grep-find-ignored-directories "node_modules")
-     (add-to-list 'grep-find-ignored-directories "dist")
+(add-to-list 'grep-find-ignored-directories "node_modules")
+(add-to-list 'grep-find-ignored-directories "dist")
+
 (add-hook 'grep-mode-hook (lambda () (toggle-truncate-lines 1)))
+(add-hook 'helm-grep-mode-hook (lambda () (toggle-truncate-lines 1)))
 
 (provide 'tmtxt-grep)
