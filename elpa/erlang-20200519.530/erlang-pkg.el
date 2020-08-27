33e6151eff61256a6eeaf68daa0556e5646975b5
Truong Tran <truong@agencyrevolution.com>
2020-08-27 17:26:49 +0700

    Update packages

---
+++ elpa/erlang-20200519.530/erlang-pkg.el
@@ -0,0 +1,6 @@
+(define-package "erlang" "20200519.530" "Erlang major mode"
+  '((emacs "24.1"))
+  :commit "b4b9b43a49c517540125c9e812919ba013eeeb01")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/jinja2-mode-20200718.730/jinja2-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jinja2-mode" "20200718.730" "A major mode for jinja2" 'nil :commit "ecd19a40b7832bb00f0a2244e3b0713d0bf3850d" :authors '(("Florian Mounier aka paradoxxxzero")) :maintainer '("Florian Mounier aka paradoxxxzero"))
