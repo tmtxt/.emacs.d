ade4a69b8891ea13d496b6a8e1d9148bae6cce71
Truong Tran <truong@agencyrevolution.com>
2020-08-28 11:45:08 +0700

    Update packages

--- config/tmtxt-key-bindings.el
+++ config/tmtxt-key-bindings.el
@@ -275,7 +275,7 @@
     global-map
   "C-S-q" 'tmtxt/helm
   "<C-M-S-iso-lefttab>"  'tmtxt/helm
-  "M-V"    'helm-show-kill-ring
+  ;; "M-V"    'helm-show-kill-ring
   "C-S-s"     'helm-occur)
 
 (tmtxt/keys 'helm helm-map
@@ -286,7 +286,7 @@
     global-map
   "C-x C-S-f"        'helm-projectile-find-file
   "C-x C-S-d"        'helm-projectile-find-dir
-  "C-x C-S-s"        'helm-projectile-switch-project
+  ;; "C-x C-S-s"        'helm-projectile-switch-project
   "C-x C-S-r"        'helm-projectile-ag)
 
 (tmtxt/keys 'helm-dired-recent-dirs
---
+++ elpa/helm-core-20200803.1032/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20200803.1032" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "3ddce439817c89d17b5969373010a0d22250016f" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
