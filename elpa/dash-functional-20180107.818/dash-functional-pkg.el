f2d5c0e9fb1e8e418ff5fc9404bb2040c79c3707
Truong Tran <truong@agencyrevolution.com>
2020-08-28 16:15:42 +0700

    Update packages

--- elpa/dash-functional-20180107.818/dash-functional-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "dash-functional" "20180107.818" "Collection of useful combinators for Emacs Lisp" '((dash "2.0.0") (emacs "24")) :commit "a74f4cfcdc8d0642a9f602ad494f0354f27dacc9" :keywords '("lisp" "functions" "combinators"))
