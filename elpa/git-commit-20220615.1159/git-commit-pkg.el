c99e041a4ae8825045e9904a6b0621f7d11ce62c
TruongTX <me@truongtx.me>
2022-06-24 20:39:09 +0700

    Upgrade magit

---
+++ elpa/git-commit-20220615.1159/git-commit-pkg.el
@@ -0,0 +1,18 @@
+(define-package "git-commit" "20220615.1159" "Edit Git commit messages."
+  '((emacs "25.1")
+    (compat "28.1.1.2")
+    (transient "20210920")
+    (with-editor "20211001"))
+  :commit "6a711f1b94abe2858ebbb57ef6b58847f21809c4" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li")
+    ("Sebastian Wiesner" . "lunaryorn@gmail.com")
+    ("Florian Ragwitz" . "rafl@debian.org")
+    ("Marius Vollmer" . "marius.vollmer@gmail.com"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/magit-section-20220615.1159/magit-section-pkg.el
@@ -0,0 +1,14 @@
+(define-package "magit-section" "20220615.1159" "Sections for read-only buffers."
+  '((emacs "25.1")
+    (compat "28.1.1.2")
+    (dash "20210826"))
+  :commit "6a711f1b94abe2858ebbb57ef6b58847f21809c4" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
