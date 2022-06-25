51bb9e76bf3045eeccceba414c5e2b88625111ce
TruongTX <me@truongtx.me>
2022-06-25 10:17:32 +0700

    Remove clojure config

--- config/tmtxt-lisp.el
+++ config/tmtxt-lisp.el
@@ -1,10 +1,6 @@
 ;;; some of the code is from emacs starter kit
 
 (require 'flycheck)
-;; (require 'clojure-mode)
-;; (require 'cider)
-;; (require 'ac-cider)
-(require 'quack)
 
 ;;; LISP & EMACS LISP
 ;;; hook
@@ -16,9 +12,6 @@
              tmtxt/add-elisp-font-lock
              tmtxt/prog-mode-setup))
   (add-hook 'emacs-lisp-mode-hook f))
-
-;;; flycheck setup
-;; (flycheck-package-setup)
 
 (defun tmtxt/remove-elc-on-save ()
   "If you're saving an elisp file, likely the .elc is no longer valid."
@@ -35,57 +28,5 @@
           (0 (progn (compose-region (match-beginning 1) (match-end 1)
                                     ,(make-char 'greek-iso8859-7 107))
                     nil))))))
-
-
-;;; CLOJURE & CLOJURESCRIPT
-;; (add-hook 'clojure-mode-hook 'tmtxt/add-clj-font-lock)
-;; (add-hook 'clojurescript-mode-hook 'tmtxt/add-clj-font-lock)
-;; (add-hook 'clojure-mode-hook 'paredit-mode)
-;; (add-hook 'clojure-mode-hook 'tmtxt/prog-mode-setup)
-
-;; (defun tmtxt/add-clj-font-lock ()
-;;   "Add font lock keywords for clojure"
-;;   (font-lock-add-keywords
-;;    nil `(("(\\(\\<fn\\>\\)"
-;;           (0 (progn (compose-region (match-beginning 1)
-;;                                     (match-end 1)
-;;                                     "\u0192"
-;;                                     'decompose-region)))))))
-
-;; (defun tmtxt/switch-to-cider-repl ()
-;;   "Switch directly to cider repl buffer if exist, otherwise, connect to a new one"
-;;   (interactive)
-;;   ;; check whether cider repl buffer exists
-;;   (let* ((cider-buffer? (lambda (buffer)
-;;                           (->> buffer
-;;                                (buffer-name)
-;;                                (s-contains? "cider-repl"))))
-;;          (cider-buffers (->> (buffer-list)
-;;                              (-filter cider-buffer?))))
-;;     (if cider-buffers
-;;         (switch-to-buffer (first cider-buffers))
-;;       (call-interactively 'cider-connect))))
-
-;; (dolist (f '(ac-flyspell-workaround
-;;              ac-cider-setup
-;;              ac-cider-setup))
-;;   (add-hook 'cider-mode-hook f))
-;; (add-to-list 'ac-modes 'cider-mode)
-;; (add-to-list 'ac-modes 'cider-repl-mode)
-
-;; (defun tmtxt/set-auto-complete-as-completion-at-point-function ()
-;;   (setq completion-at-point-functions '(auto-complete)))
-;; (add-hook 'auto-complete-mode-hook 'tmtxt/set-auto-complete-as-completion-at-point-function)
-;; (add-hook 'cider-mode-hook 'tmtxt/set-auto-complete-as-completion-at-point-function)
-
-
-;; SCHEME
-;; The binary of your interpreter
-(setq scheme-program-name "mit-scheme")
-;; This hook lets you use your theme colours instead of quack's ones.
-(defun tmtxt/scheme-mode-quack-hook ()
-  (setq quack-fontify-style 'emacs))
-(add-hook 'scheme-mode-hook 'tmtxt/scheme-mode-quack-hook)
-;; (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
 
 (provide 'tmtxt-lisp)
--- elpa/quack-20181106.1301/quack-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "quack" "20181106.1301" "enhanced support for editing and running Scheme code" 'nil :commit "2146805ce2b5a9b155d73929986f11e713787e26")
