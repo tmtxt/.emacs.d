6225362fd07bb4a2cd1df665ef33d0a3791aca74
TruongTX <me@truongtx.me>
2022-06-25 10:24:42 +0700

    Remove python config

--- config/tmtxt-python.el
+++ config/tmtxt-python.el
@@ -1,19 +1,4 @@
 ;;; remove trailing white space since python uses indentation for code block
 (add-hook 'python-mode-hook 'tmtxt/prog-mode-setup)
 
-;;; syntax checking using pyflakes
-;; (require 'flycheck-pyflakes)
-;; (add-hook 'python-mode-hook 'flycheck-mode)
-;; (add-to-list 'flycheck-disabled-checkers 'python-flake8)
-;; (add-to-list 'flycheck-disabled-checkers 'python-pylint)
-
-;;; elpy
-;; (require 'elpy)
-;; (setq elpy-modules
-;;       (delete 'elpy-module-flymake elpy-modules))
-;; (add-hook 'elpy-mode-hook (tmtxt/on-fn 'flycheck-mode))
-;; (setq elpy-rpc-backend "jedi")
-;; (elpy-enable)
-;; (elpy-use-ipython)
-
 (provide 'tmtxt-python)
--- elpa/flycheck-pyflakes-20170330.2311/flycheck-pyflakes-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "flycheck-pyflakes" "20170330.2311" "Support pyflakes in flycheck" '((flycheck "0.18")) :commit "61b045939e3743b2162b7e4e73249c66fc2b8f65" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk"))
