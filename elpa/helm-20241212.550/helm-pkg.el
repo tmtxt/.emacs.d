8cacaa46abfd62baf4991155967ebc5cd7117b69
Tony Tran <tony.tran@wisetechglobal.com>
2024-12-13 09:15:33 +1100

    Upgrade helm

---
+++ elpa/helm-20241212.550/helm-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm" "20241212.550"
+  "Helm is an Emacs incremental and narrowing framework."
+  '((helm-core "4.0")
+    (wfnames   "1.2"))
+  :url "https://github.com/emacs-helm/helm"
+  :commit "2abfa1d53d5bbe0086bd08a0a001f52ea3cedc47"
+  :revdesc "2abfa1d53d5b"
+  :keywords '("helm" "convenience" "files" "buffers" "grep" "completion" "lisp" "matching" "tools" "help")
+  :authors '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers '(("Thierry Volpiatto" . "thievol@posteo.net")))
---
+++ elpa/helm-core-20241212.610/helm-core-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "helm-core" "20241212.610"
+  "Development files for Helm."
+  '((emacs "25.1")
+    (async "1.9.8"))
+  :url "https://github.com/emacs-helm/helm"
+  :commit "41c2aaccf08a6499c4ffef2b90a4248380a63731"
+  :revdesc "41c2aaccf08a"
+  :authors '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers '(("Thierry Volpiatto" . "thievol@posteo.net")))
