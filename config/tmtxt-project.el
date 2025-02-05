52fd5505432e8a0731eaef9c35a99b8eb036acd3
Tony Tran <tony.tran@wisetechglobal.com>
2025-02-05 12:07:10 +1100

    Comment code

--- config/tmtxt-project.el
+++ config/tmtxt-project.el
@@ -10,17 +10,17 @@
  projectile-enable-caching t)
 
 ;;; ----
-(defun find-csproj-containing-folders-recursively (directory)
-  "Find all folders containing .csproj files in DIRECTORY recursively."
-  (let ((result '()))
-    (dolist (file (directory-files-recursively directory "\\.csproj$"))
-      (let ((folder (file-name-directory file)))
-        (unless (member folder result)
-          (setq result (append result (list folder))))))
-    result))
+;; (defun find-csproj-containing-folders-recursively (directory)
+;;   "Find all folders containing .csproj files in DIRECTORY recursively."
+;;   (let ((result '()))
+;;     (dolist (file (directory-files-recursively directory "\\.csproj$"))
+;;       (let ((folder (file-name-directory file)))
+;;         (unless (member folder result)
+;;           (setq result (append result (list folder))))))
+;;     result))
 
-(dolist (d (find-csproj-containing-folders-recursively "c:/git/wtg/eServices/eServices"))
-  (projectile-add-known-project d)
-  )
+;; (dolist (d (find-csproj-containing-folders-recursively "c:/git/wtg/eServices/eServices"))
+;;   (projectile-add-known-project d)
+;;   )
 
 (provide 'tmtxt-project)
